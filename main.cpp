f0d2f350cde91d2a0962f5d3e6cf91c171822387
Coding-Ocean <eijo524@gmail.com>
2023-11-24 17:11:46 +0900

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+int main()
+{
+	//name02();
+	//name04();
+	//name05();
+	//name08();
+	//name09();
+	//name10();
+	//name13();
+	//name17();
+	//name18();
+	//name22();
+	//name24();
+	//name25();
+	//name27();
+	//name28();
+	//name29();
+	return 0;
+}
